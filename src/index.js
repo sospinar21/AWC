eb69e30b281a5d67e8105b602315ed68f74d12f0
sospinar21 <sospinar21@gmail.com>
2018-05-23 18:25:16 -0600

    first commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App/App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { rootReducer } from './Reducers/index';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && 
+                 window.__REDUX_DEVTOOLS_EXTENSION__();
+const store = createStore(rootReducer, devTools);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>, document.getElementById('root'));
