acb322ebe9be16d21fb264daa5c183f188251997
sospinar21 <sospinar21@gmail.com>
2018-05-28 16:29:08 -0600

    Add forum cards

--- src/Components/Home/Community/Community.js
+++ src/Components/Home/Community/Community.js
@@ -4,22 +4,25 @@ import './Community.css'
 export class Community extends Component {
   
   displayForum = () => {
-    return (
-      <div className='post'>
-        <div className='post-img'>
-          <img href='https://img.grouponcdn.com/deal/uertuP3ir2AGAqWFpKGp/jq-2048x1228/v1/c700x420.jpg'/>
-        </div>
-        <div className='post-description'>
-          <div className='text-description'> 
-            <p>Belly dance is primarily a torso-driven dance, with an emphasis on articulations of the hips.[9] Unlike many Western dance forms, the focus of the dance is on isolations of the torso muscles, rather than on movements of the limbs through space. Although some of these isolations appear similar to the isolations used in jazz ballet, they are sometimes driven differently and have a different feeling or emphasis.</p>
+    const arrp = [1,2,3,4,5,6]
+    const posts = arrp.map((post, index) => {
+      return (
+        <div className='post' key={index}>
+          <div className='post-img'>
           </div>
-          <div className='likes'>
-            <i class="material-icons">thumb_up_alt</i>
-            <i class="material-icons">thumb_down_alt</i>
+          <div className='post-description'>
+            <div className='text-description'> 
+              <p>Belly dance is primarily a torso-driven dance, with an emphasis on articulations of the hips.[9] Unlike many Western dance forms, the focus of the dance is on isolations of the torso muscles, rather than on movements of the limbs through space. Although some of these isolations appear similar to the isolations used in jazz ballet, they are sometimes driven differently and have a different feeling or emphasis.</p>
+            </div>
+            <div className='likes'>
+              <i className="material-icons icon">thumb_up_alt</i>
+              <i className="material-icons icon">thumb_down_alt</i>
+            </div>
           </div>
         </div>
-      </div>
-    )
+      )
+    })
+    return posts
   }
 
 
