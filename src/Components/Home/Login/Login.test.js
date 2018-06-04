83fc9f0a39669fc4c8f83b453836070bc8bc12db
sospinar21 <sospinar21@gmail.com>
2018-06-04 07:51:37 -0600

    add tests for log in

--- src/Components/Home/Login/Login.test.js
+++ src/Components/Home/Login/Login.test.js
@@ -6,14 +6,47 @@ import { mount, shallow } from 'enzyme';
 describe('login', () => {
 
   let login
+  let mockProps;
 
   beforeEach(() => {
 
-    login = mount(<Login /> , { disableLifecycleMethods: true })
+    mockProps = {
+      suggestedEvents: [{name: 'party'}],
+      user: {namename:'Steph'},
+      addUser: jest.fn(),
+    }
+
+    login = shallow(<Login {...mockProps}/> , { disableLifecycleMethods: true })
 
   })
 
   it('matches snapshot', () => {
     expect(login).toMatchSnapshot();
   })
+
+  // describe('handleInputChange', () => {
+
+  //   it('updates state when input changes', () => {
+
+  //     let input = login.find('.email-si');
+
+  //     input.simulate('change', { target: { value: 'a@a.me' } });
+
+  //     expect(login.state().email).toEqual('a@a.me');
+  //   })
+    
+  // })
+
+  // describe('userSignIn', () => {
+
+  //   it('sets the state with the new users email and psw', () => {
+
+  //     login.state().email = ''
+
+  //     login.instance().userSignIn()
+
+  //     expect(login.state().email).toEqual('a@a.me');
+  //   })
+    
+  // })
 })
