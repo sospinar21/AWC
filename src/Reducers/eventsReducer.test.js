b0a95ca38f64e6730f565151a9659d390ae6ffb7
sospinar21 <sospinar21@gmail.com>
2018-06-04 09:17:12 -0600

    test reducer

---
+++ src/Reducers/eventsReducer.test.js
@@ -0,0 +1,22 @@
+import { eventsReducer } from './eventsReducer';
+
+describe('favorites reducer', () => {
+  it('returns default state if the action type doesn\'t match', () => {
+    let initialState = {};
+    
+    expect(eventsReducer(undefined, {})).toEqual(initialState);
+  });
+
+  it('returns state with new events', () => {
+    let initialState = [];
+    let eventsData = {name: 'workshop'};
+    let addEventAction = {
+      type: 'ADD_EVENTS',
+      eventsData
+    };
+
+    let newState = eventsReducer(initialState, addEventAction);
+
+    expect(newState).toEqual(eventsData);
+  });
+});
