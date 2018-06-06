f1c4fe13a7cd0b4ebcbdf26eb33f2cdec6755cc5
sospinar21 <sospinar21@gmail.com>
2018-06-06 09:45:08 -0600

    test apicalls

--- src/Helper/ApiCalls/ApiCalls.js
+++ src/Helper/ApiCalls/ApiCalls.js
@@ -1,67 +1,96 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-class ApiCalls extends Component {
-  constructor(){
-    super();
-  }
+class ApiCalls {
 
   fetchEvents = async (city) => {
-    const url = `https://api.awc.dance/events?city=${city}`;
-    const response = await fetch(url);
-    const data= await response.json();
-    return data;
+    try {
+      const url = `https://api.awc.dance/events?city=${city}`;
+      const response = await fetch(url);
+      const data= await response.json();
+      return data;}
+    catch (error) {
+      throw new Error(error.message);
+    }
   } 
 
   fetchStudios = (city) => {
-    const url = `https://api.awc.dance/?city=${city}`;
-    return fetch(url, {
-      method: 'GET',
-      headers: {
-        'Content-type': 'application/json'
-      }
-    }).then( response => response.json() )
-      .then( data => data );   
+    try {
+      const url = `https://api.awc.dance/?city=${city}`;
+      return fetch(url, {
+        method: 'GET',
+        headers: {
+          'Content-type': 'application/json'
+        }
+      }).then( response => response.json() )
+        .then( data => data )}
+    catch (error) {
+      throw new Error(error.message);
+    }      
   }
 
   fetchVideos= async () => {
-    var url = 'https://api.awc.dance/youtubevideos';
-    const response = await fetch(url);
-    const data = await response.json();
-    return data.items;
+    try {
+      var url = 'https://api.awc.dance/youtubevideos';
+      const response = await fetch(url);
+      const data = await response.json();
+      return data.items;
+    }
+    catch (error) {
+      throw new Error(error.message);
+    }
   }
 
   fetchGoogle = async (userInput) => {
-    const url = `https://api.awc.dance/autocomplete?input=${userInput}`;
-    const response = await fetch(url);
-    const data = await response.json();
-
-    return data;
+    try {
+      const url = `https://api.awc.dance/autocomplete?input=${userInput}`;
+      const response = await fetch(url);
+      const data = await response.json();
+      return data;
+    } 
+    catch (error) {
+      throw new Error(error.message);
+    }
   }
 
   fetchSingleStudio = async (studioId) => {
-    const url = `https://api.awc.dance/single-studio?id=${studioId}`;
-    const response = await fetch(url);
-    const data = await response.json();
-    return data;
+    try {
+      const url = `https://api.awc.dance/single-studio?id=${studioId}`;
+      const response = await fetch(url);
+      const data = await response.json();
+      return data;
+    }
+    catch (error) {
+      throw new Error(error.message);
+    }
   }
 
   postComment = async (user, input, category) => {
-    const userEmail = user.username;
-    const url = `https://api.awc.dance/postcomment?name=${userEmail}&content=${input}&type=${category}`;
-    const response = await fetch(url);
-    const data = await response.json();
-    return data;
+    try {
+      const userEmail = user.username;
+      const url = `https://api.awc.dance/postcomment?name=${userEmail}&content=${input}&type=${category}`;
+      const response = await fetch(url);
+      const data = await response.json();
+      return data;
+    }
+    catch (error) {
+      throw new Error(error.message);
+    }
 
   }
 
   getPosts = async () => {
-    const url = 'https://api.awc.dance/getposts';
-    const response = await fetch(url)
-    const data = await response.json()
-    return data
+    try {
+      const url = 'https://api.awc.dance/getposts';
+      const response = await fetch(url)
+      const data = await response.json()
+      return data
+    }
+    catch (error) {
+      throw new Error(error.message);
+    }
   }
 
-  addUserInfo 
+
 }
 
 export default ApiCalls;
--- src/Helper/ApiCalls/ApiCalls.test.js
+++ src/Helper/ApiCalls/ApiCalls.test.js
@@ -1,34 +1,100 @@
 import ApiCalls from '../ApiCalls/ApiCalls'
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { shallow } from 'enzyme';
+import { mount, shallow } from 'enzyme';
+
+describe('apiCalls', () => {
+  let apiCalls = new ApiCalls();
+
+  beforeEach(() => {
+
+    window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+      status: 200,
+      json: () => Promise.resolve({data: {}})
+    }));
 
 
-describe('app', () => {
-
-  it('matches snapshot', () => {
-    let apiCalls = shallow(<ApiCalls />, { disableLifecycleMethods: true })
-    expect(apiCalls).toMatchSnapshot();
-  })
+  });
 
   describe('fetchEvents', () => {
+    
+    describe('fetchEvents if the status is ok', () => {
+      it('calls fetchEvents with the correct params', () => {
+        
+        const city = 'Denver'
+        const expected = 'https://api.awc.dance/events?city=Denver'
 
-    it('', () => {
+        apiCalls.fetchEvents(city);
 
+        expect(window.fetch).toHaveBeenCalledWith(expected)
+      });
     })
-  })
+
+    describe('fetchEvents if the status is not ok', () => {
+
+      it('throws an error if fetch fails', async () => {
+        window.fetch = jest
+          .fn()
+          .mockImplementation(() => Promise.reject(new Error('Error')));
+        const expected = new Error('Error');
+    
+        await expect(apiCalls.fetchEvents()).rejects.toEqual(expected);
+      });
+    });
+  });
 
   describe('fetchStudios', () => {
+    
+    describe('fetchStudios if the status is ok', () => {
+      it('calls window.fetch', () => {
+        
+        const city = 'Denver'
 
-    it('', () => {
-      
+        apiCalls.fetchStudios(city);
+
+        expect(window.fetch).toHaveBeenCalled()
+      });
     })
-  })
 
+    describe('fetchStudios if the status is not ok', () => {
+
+      it('throws an error if fetch fails', async () => {
+        window.fetch = jest
+          .fn()
+          .mockImplementation(() => Promise.reject(new Error('Error')));
+        const expected = new Error('Error');
+    
+        await expect(apiCalls.fetchStudios()).rejects.toEqual(expected);
+      });
+    });
+  });
+  
   describe('fetchVideos', () => {
+    
+    describe('fetchVideos if the status is ok', () => {
+      it('calls window.fetch with the correct params', () => {
 
-    it('', () => {
-      
+        const expected = 'https://api.awc.dance/youtubevideos'
+
+        apiCalls.fetchVideos();
+
+        expect(window.fetch).toHaveBeenCalledWith(expected)
+      });
     })
-  })
-})
+
+    describe('fetchEvents if the status is not ok', () => {
+
+      it('throws an error if fetch fails', async () => {
+        window.fetch = jest
+          .fn()
+          .mockImplementation(() => Promise.reject(new Error('Error')));
+        const expected = new Error('Error');
+    
+        await expect(apiCalls.fetchVideos()).rejects.toEqual(expected);
+      });
+    });
+  });
+
+ 
+
+});
